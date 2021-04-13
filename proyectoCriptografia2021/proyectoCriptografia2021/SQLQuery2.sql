202dda5644e20a2fd79928e56333fa087ee4e0e4
AlinaCumber <60980420+AlinaCumber@users.noreply.github.com>
2021-04-13 16:57:42 +0300

    13.04
    Classe 1

---
+++ proyectoCriptografia2021/proyectoCriptografia2021/SQLQuery2.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE MENSAJE(
+IdMensaje int not null primary key identity,
+mensajeOriginal varchar(MAX),
+mensajeCifrado varchar(MAX),
+idMetodoCifrado int,
+foreign key (idMetodoCifrado) REFERENCES Metodo_Cifrado(idCifrado)
+)
