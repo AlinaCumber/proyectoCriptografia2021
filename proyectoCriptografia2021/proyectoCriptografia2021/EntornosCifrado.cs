ac5c68a21c01c6a0748183a1c200805d3b043ef3
AlinaCumber <60980420+AlinaCumber@users.noreply.github.com>
2021-04-21 21:47:23 +0300

    drf
    we

---
+++ proyectoCriptografia2021/proyectoCriptografia2021/EntornosCifrado.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoCriptografia2021
+{
+    public class EntornosCifrado
+    {
+        SqlConnection miConnexion;
+
+        public EntornosCifrado()
+        {
+            miConnexion = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CRIPTOGRAFIA;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            miConnexion.Open();
+
+        }
+
+        public SqlDataReader consultarBD (String consulta)
+        {
+            SqlDataReader resultados;
+            SqlCommand comando;
+
+            comando = new SqlCommand(consulta, miConnexion);
+
+            resultados = comando.ExecuteReader();
+
+
+            return resultados;
+        }
+
+        public string cifrarMensaje (string mensajeOriginal, int idMetodoCifrado)
+        {
+            int position = 0;
+            int positionLetraAcrual = 0;
+            int positionLetraNueva = 0;
+            int longitudAlfabeto = 27;
+
+            string mensajeCifrado;
+
+            SqlDataReader resultadoBD;
+
+            resultadoBD = consultarBD("select position from Metodo_Cifrado where idCifrado=" + idMetodoCifrado);
+
+            while (resultadoBD.Read())
+            {
+                position = (int)resultadoBD[0];
+            }
+
+            for(int i =0; i < mensajeOriginal.Length; i++)
+        }
+
+    }
+}
--- proyectoCriptografia2021/proyectoCriptografia2021/Form1.Designer.cs
+++ proyectoCriptografia2021/proyectoCriptografia2021/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cMetodoCifrado = new System.Windows.Forms.ComboBox();
+            this.cMensajeCifrado = new System.Windows.Forms.RichTextBox();
+            this.bBorrar = new System.Windows.Forms.Button();
+            this.bGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(940, 556);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Metodos de Cifrado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mensaje a Cifrar";
+            // 
+            // cMetodoCifrado
+            // 
+            this.cMetodoCifrado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cMetodoCifrado.FormattingEnabled = true;
+            this.cMetodoCifrado.ItemHeight = 29;
+            this.cMetodoCifrado.Location = new System.Drawing.Point(395, 92);
+            this.cMetodoCifrado.Name = "cMetodoCifrado";
+            this.cMetodoCifrado.Size = new System.Drawing.Size(429, 37);
+            this.cMetodoCifrado.TabIndex = 3;
+            // 
+            // cMensajeCifrado
+            // 
+            this.cMensajeCifrado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cMensajeCifrado.Location = new System.Drawing.Point(395, 193);
+            this.cMensajeCifrado.Name = "cMensajeCifrado";
+            this.cMensajeCifrado.Size = new System.Drawing.Size(429, 182);
+            this.cMensajeCifrado.TabIndex = 4;
+            this.cMensajeCifrado.Text = "";
+            // 
+            // bBorrar
+            // 
+            this.bBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBorrar.Location = new System.Drawing.Point(395, 417);
+            this.bBorrar.Name = "bBorrar";
+            this.bBorrar.Size = new System.Drawing.Size(137, 54);
+            this.bBorrar.TabIndex = 5;
+            this.bBorrar.Text = "Borrar";
+            this.bBorrar.UseVisualStyleBackColor = true;
+            // 
+            // bGuardar
+            // 
+            this.bGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bGuardar.Location = new System.Drawing.Point(696, 417);
+            this.bGuardar.Name = "bGuardar";
+            this.bGuardar.Size = new System.Drawing.Size(128, 54);
+            this.bGuardar.TabIndex = 6;
+            this.bGuardar.Text = "Guardar";
+            this.bGuardar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(940, 556);
+            this.Controls.Add(this.bGuardar);
+            this.Controls.Add(this.bBorrar);
+            this.Controls.Add(this.cMensajeCifrado);
+            this.Controls.Add(this.cMetodoCifrado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cMetodoCifrado;
+        private System.Windows.Forms.RichTextBox cMensajeCifrado;
+        private System.Windows.Forms.Button bBorrar;
+        private System.Windows.Forms.Button bGuardar;
     }
 }
 
